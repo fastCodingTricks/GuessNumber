e888a1994f2842d5acf2e79b24d4929ae65b75fa
MasihMuhammadi <masihmuhammadi202@gmail.com>
2023-10-02 16:45:02 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+    const guessInput = document.querySelector("input");
+    const checkButton = document.querySelector("button");
+    const message = document.querySelector("#message");
+
+    let random = Math.floor(Math.random() * 100) // random number between 1 and 100;
+    let attempts = 0;
+
+    checkButton.addEventListener("click", () =>{
+        let guess = parseInt(guessInput.value);
+        attempts++;
+        if(isNaN(guess) || guess < 1 || guess > 100){
+            message.textContent = "please enter a number between 1 and 100";
+        }
+        else if(guess  === random){
+            message.textContent = "congratulations, you can find the " + random + "number in" +  attempts + "attempts";
+        }
+        else{
+            guess < random ? message.textContent = "try a higher number" : message.textContent = "try a lower number";
+        }
+        guessInput.value = "";
+    })
+})
